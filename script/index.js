83f9f7cb642dc6b1f644654b9bbe6eeefcc8ceb2
SnakeorSnack <118553876+SnakeorSnack@users.noreply.github.com>
2023-05-21 16:48:59 +0200

    added initial cards

--- script/index.js
+++ script/index.js
@@ -6,6 +6,38 @@ const inputName = infoEditorPopup.querySelector('.popup__inputfield_change_name'
 const inputProfession = infoEditorPopup.querySelector('.popup__inputfield_change_profession');
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
+const gridListElement = document.querySelector('.photo-grid__list');
+const cardTemplate = document.querySelector('.card-template').content;
+
+/* Массив карточек «из коробки» */
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+/* Функция открытия попапа редактирования профиля */
 
 editButton.addEventListener('click',openEditor);
 function openEditor() {
@@ -13,11 +45,14 @@ function openEditor() {
   inputName.value = profileName.textContent;
   inputProfession.value = profileProfession.textContent;
 }
+/* Функция закрытия попапа */
 
 closePopupButton.addEventListener('click',closePopup);
 function closePopup() {
   infoEditorPopup.classList.remove('popup_opened');
 }
+
+/* Функция сохранения изменений Имени и Профессии */
 
 infoEditorForm.addEventListener('submit', clickSubmitButton);
 function clickSubmitButton(event) {
@@ -28,3 +63,12 @@ function clickSubmitButton(event) {
   profileProfession.textContent = profession;
   closePopup();
 }
+
+/* Функция добавления карточек «из коробки» */
+
+initialCards.forEach((item) => {
+  const templateElement = cardTemplate.cloneNode(true);
+  templateElement.querySelector('.photo-grid__picture-name').innerText = item.name;
+  templateElement.querySelector('.photo-grid__picture').src = item.link;
+  gridListElement.append(templateElement);
+});
